4686f09d5edb53b30c8625f1335ce09042cb30ff
leonanto <leonardo.a.grant@gmail.com>
2015-11-22 20:33:04 +0000

    XML fixed

--- world.js
+++ world.js
@@ -2,10 +2,10 @@ window.onload = function(){
     document.getElementById("lookup").addEventListener(
         "click", function(){
              var text = document.getElementById("term").value;
-             var check = document.getElementById("check").value;
-             //console.log(check+""+text+""+"all");
-            if(text == "" && check == "all"){
-               new Ajax.Request("world.php?lookup="+text+"&all=true",
+             var check = document.getElementById("check").checked;
+             console.log(check+""+text);
+            if(text == "" && check == true){
+               new Ajax.Request("world.php?all=true&format=xml",
                {
                     method: "get",
                     onSuccess: showResults
@@ -20,7 +20,7 @@ window.onload = function(){
             } 
         });
         function showResults(data){
-            alert(data.responseText);
+            alert(data.responseXML);
             //var div = document.getElementById("result");
             //div.innerHTML = div.innerHTML + data.responseText; 
         }
--- world.php
+++ world.php
@@ -5,32 +5,47 @@ getenv('C9_USER')
 );
 mysql_select_db("world");
 
-
 $LOOKUP = $_REQUEST['lookup'];
 $ALL = $_REQUEST['all'];
+$FORMAT = $_REQUEST['format'];
 
-if($LOOKUP == null && $ALL == false){
-  return 0;
-}
-
-if($LOOKUP == null && $ALL == true){
-  $results = mysql_query("SELECT * FROM countries WHERE name LIKE '%$LOOKUP%';");
-  print $results;
-  # loop through each country
-  while ($row = mysql_fetch_array($results)) {
-    ?>
-    <li> <?php echo $row["name"]; ?>, ruled by <?php echo $row["head_of_state"]; ?> </li>
-    <?php
-  }  
-}
 
 # execute a SQL query on the database
+ //$results = $db->execute("SELECT * FROM countries WHERE name LIKE '%$LOOKUP%';");
 $results = mysql_query("SELECT * FROM countries WHERE name LIKE '%$LOOKUP%';");
 print $results;
-# loop through each country
-while ($row = mysql_fetch_array($results)) {
-  ?>
-  <li> <?php echo $row["name"]; ?>, ruled by <?php echo $row["head_of_state"]; ?> </li>
-  <?php
+
+if($LOOKUP == null && $ALL == 'false'){
+  return 0;
 }
+elseif($ALL == 'true' && $FORMAT =='xml' || $LOOKUP != null){
+  $string='<?xml version="1.0" encoding="utf-8"?> <ul> <countrydata>';
+  while ( $row = mysql_fetch_array($results) ) {
+    $string.='<li>';
+    $string.='<country>';
+    $string.='<name>';
+    $string.=$row["name"];
+    $string.='</name>';
+    $string.='<ruler>';
+    $string.=$row["head_of_state"];
+    $string.='</ruler>';
+    $string.='</country>';
+    $string.='</li>';
+  }
+  $string.='</countrydata></ul>';
+  
+  $ok = utf8_encode($string);
+  $xml=new SimpleXMLElement($ok);
+  echo $xml -> asXML();
+  //echo $string;
+}
+// else{
+//   //$LOOKUP = $_REQUEST['lookup'];
+//   //$results = mysql_query("SELECT * FROM countries WHERE name LIKE '%$LOOKUP%';");
+//   while ($row = mysql_fetch_array($results)) {
+//     ?>
+//     <li> <?php echo $row["name"]; ?>, ruled by <?php echo $row["head_of_state"]; ?> </li>
+//     <?php
+//   }
+// }
 ?>
